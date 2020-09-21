0aee9b358f9a1ebbb121364835003502df7c2395
SlamDunk <stepanov.arkadyushka@inbox.ru>
2020-09-21 22:18:17 +0400

    New Change in project

--- ChangeTheme.js
+++ ChangeTheme.js
@@ -5,13 +5,17 @@ ChangeButton.addEventListener('click', () => {
    if(!document.documentElement.classList.contains('change-theme')) {
    	  document.documentElement.classList.add('change-theme');
 
+      trans();
+
    	  ChangeButton.children[0].className = 'fas fa-moon';
 
    	  ChangeButton.children[0].classList.add('change-shadow');
 
    	  ChangeImg.classList.add('change-img-shadow');
    } else {
-   	  document.documentElement.classList.remove('change-theme');
+        document.documentElement.classList.remove('change-theme');
+
+        trans();
 
    	  ChangeButton.children[0].className = 'fas fa-sun';
 
@@ -20,3 +24,11 @@ ChangeButton.addEventListener('click', () => {
    	  ChangeImg.classList.remove('change-img-shadow');
    }
 });
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+
+    setTimeout(() => {
+       document.documentElement.classList.remove('transition');
+    }, 900);
+};
